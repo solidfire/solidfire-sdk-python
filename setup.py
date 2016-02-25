3039c8c8d6d6c35bf4189e44ae71998dffe6b528
Jason Ryan Womack <jason.womack@solidfire.com>
2016-02-25 06:09:34 -0700

    SDK-186 - extra_require cleanup and docs requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 """SolidFire Python SDK"""
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 import os
 from distutils import dir_util
-from distutils import log
 from distutils.command.clean import clean as _clean
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Get the long description from the README file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -36,22 +32,14 @@ setup(
     description='SolidFire Python SDK',
     long_description=long_description,
 
-    # The project's main homepage.
     url='https://github.com/solidfire/solidfire-sdk-python',
 
-    # Author details
     author='Jason Ryan Womack',
     author_email='jason.womack@solidfire.com',
 
-    # Choose your license
     license='Apache2',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
 
         'Operating System :: OS Independent',
@@ -78,46 +66,20 @@ setup(
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=['pycurl>=7.19.0', 'future>=0.15.2'],
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
+    # $ pip install -e ".[dev,test, docs, release]"
     extras_require={
         'dev': ['check-manifest'],
-        'test': ['coverage', 'pyhamcrest>=1.8.5', 'pytest>=2.8.7', 'pytest-flake8>=0.1'],
+        'test': [
+            'coverage',
+            'pyhamcrest>=1.8.5',
+            'pytest>=2.8.7',
+            'pytest-flake8>=0.1'
+        ],
+        'docs': ['Sphinx>=1.3.5', 'sphinx_rtd_theme>=0.1.9', ],
+        'release': ['twine>=1.6.5', ],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
 
     cmdclass={
         'clean': clean,
