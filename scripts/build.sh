e6746533a1ca15137aae676423be59867b42ef46
Jason Ryan Womack <jason.womack@solidfire.com>
2016-03-29 15:10:58 -0600

    Debug gh-pages commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,8 +16,6 @@ fi
 git clone -b  gh-pages https://${GH_TOKEN}@github.com/solidfire/solidfire-sdk-python.git ../solidfire-sdk-python.gh-pages
 
 # copy generated HTML site to "master" branch
-ls
-cat ../solidfire-sdk-python.gh-pages/front.yml ./README.rst
 cat ../solidfire-sdk-python.gh-pages/front.yml ./README.rst > ../solidfire-sdk-python.gh-pages/index.md
 
 # commit and push generated content to `master' branch
@@ -27,4 +25,4 @@ git config user.email "jason.womack@solidfire.com"
 git config user.name "Jason Ryan Womack"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin gh-pages > /dev/null 2>&1
+git push origin gh-pages
