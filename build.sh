13f0c57fb3c5d952ac200ca2ebfbd6d95e21e9a5
Jason Ryan Womack <jason.womack@solidfire.com>
2016-03-29 10:38:56 -0600

    SDK-205 - get auto gh-pages updates working + platform builds on travis

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# enable error reporting to the console
+set -ev
+
+# only proceed script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+#clone "master" branch of the repository using encrypted GH_TOKEN for authentification
+git clone -b  gh-pages https://${GH_TOKEN}@github.com/solidfire/solidfire-sdk-python.git ../solidfire-sdk-python.gh-pages
+
+# copy generated HTML site to "master" branch
+cat ../solidfire-sdk-python.gh-pages/front.yml ./README.rst > ../solidfire-sdk-python.gh-pages/index.md
+
+# commit and push generated content to `master' branch
+# since repository was cloned in write mode with token auth - we can push there
+cd ../solidfire-sdk-python.gh-pages
+git config user.email "jason.womack@solidfire.com"
+git config user.name "Jason Ryan Womack"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin gh-pages > /dev/null 2>&1
