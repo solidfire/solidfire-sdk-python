988469958b54c23968316d94f475595a4dc5da52
Jason Ryan Womack <jason.womack@solidfire.com>
2016-05-02 13:11:54 -0600

    1.1.0.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.1.0.1',
+    version='1.1.0.2',
 
     description='SolidFire Python SDK',
     long_description=long_description,
