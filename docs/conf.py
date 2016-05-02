4b3f52301171502955f08d1dc30d754510a57a59
Jason Ryan Womack <jason.womack@solidfire.com>
2016-05-02 13:06:10 -0600

    Bump version to

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2016, NetApp, Inc.  All Rights Reserved.'
 author = 'Jason Ryan Womack'
-version = '1.1.0.0'
-release = '1.1.0.0'
+version = '1.1.0.1'
+release = '1.1.0.1'
 language = 'en'
 
 exclude_patterns = ['_build']
