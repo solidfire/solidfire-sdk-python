6d4f36df3012ca74956eb59d2d10d9de914508c8
Build Automation <dan.audette@solidfire.com>
2016-06-13 13:33:42 -0600

    Bump version to 1.1.0.36

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2016, NetApp, Inc.  All Rights Reserved.'
 author = 'Jason Ryan Womack'
-version = '1.1.0.35'
-release = '1.1.0.35'
+version = '1.1.0.36'
+release = '1.1.0.36'
 language = 'en'
 
 exclude_patterns = ['_build']
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.1.0.35',
+    version='1.1.0.36',
 
     description='SolidFire Python SDK',
     long_description=long_description,
