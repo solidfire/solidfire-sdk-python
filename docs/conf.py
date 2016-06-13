d75b407d57bfdd617a3cb1b58238bd035f44e6fb
Build Automation <dan.audette@solidfire.com>
2016-06-13 13:43:41 -0600

    Bump version to 1.1.0.38

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2016, NetApp, Inc.  All Rights Reserved.'
 author = 'Jason Ryan Womack'
-version = '1.1.0.37'
-release = '1.1.0.37'
+version = '1.1.0.38'
+release = '1.1.0.38'
 language = 'en'
 
 exclude_patterns = ['_build']
