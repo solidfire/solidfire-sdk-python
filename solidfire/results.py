92782097955f3df68bfd3dd61d9274131fa8f277
Jason Ryan Womack <jason.womack@solidfire.com>
2016-08-09 10:35:22 -0600

    SDK-321 - CHAPSecret auto-generate

--- solidfire/results.py
+++ solidfire/results.py
@@ -51,6 +51,7 @@ from solidfire.models import VolumeAccessGroupLunAssignments
 from solidfire.models import VolumeStats
 
 
+
 class AddDrivesResult(data_model.DataObject):
     """
     The object returned by the \"add_drives\" API Service call.
