a95d2a886ba5cf4d5a31705f481043ff786e5758
Jason Ryan Womack <jason.womack@solidfire.com>
2016-09-13 12:17:09 -0600

    Bump version to 1.1.0.86

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.1.0.85',
+    version='1.1.0.86',
 
     description='SolidFire Python SDK',
     long_description=long_description,
