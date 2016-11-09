2869b26c736c463de97051b93bbdae679f33095a
Ariel Hoffman <ariel.hoffman@netapp.com>
2016-11-09 14:53:01 -0700

    Removes .idea from the commit. Also re-removes my personal text.

--- solidfire/mytest.py
+++
@@ -1,9 +0,0 @@
-from solidfire.factory import ElementFactory
-from uuid import UUID
-from solidfire.custom.models import CHAPSecret
-
-
-ef = ElementFactory.create("10.117.60.15", "admin", "admin", version=9.0, port=442)
-
-#print(ef.create_storage_container("name"))
-print(ef.list_storage_containers())
