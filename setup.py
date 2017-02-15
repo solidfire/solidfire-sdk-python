7e6c2bbeb93f3bb90ac9df42f5d9b3ffc0610690
Build Automation <dan.audette@solidfire.com>
2017-02-15 16:38:30 -0700

    Bump version to 1.2.0.127

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.2.0.126',
+    version='1.2.0.127',
 
     description='SolidFire Python SDK',
     long_description=long_description,
