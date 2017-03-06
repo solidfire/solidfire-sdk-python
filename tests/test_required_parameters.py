873e989e496a4be53320a4aad7888c46eb08a3d5
Adam Haid <adam.haid@solidfire.com>
2017-03-06 16:06:43 -0700

    bump version to 1.2.2.30 for hotfix

---
+++ tests/test_required_parameters.py
@@ -0,0 +1,11 @@
+from solidfire.common import ApiServerError
+
+from solidfire.factory import ElementFactory
+from tests.base_test import SolidFireBaseTest
+
+
+class TestRequiredParameters(SolidFireBaseTest):
+    def test_paved_road(self):
+        e = ElementFactory.create(self.cluster, self.user, self.pwd)
+        with self.assertRaises(ApiServerError):
+            e.add_account(username=None)
