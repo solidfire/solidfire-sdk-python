4dc27374d32c33d7883eb36a0fb39e086daa7c5e
Build Automation <dan.audette@solidfire.com>
2017-04-04 11:56:02 -0600

    {"pythonVersion":"1.2.2.153", "descriptorVersion":"1.0.0.40", "generatorVersion": "1.0.0.7"}

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.2.2.152',
+    version='1.2.2.153',
 
     description='SolidFire Python SDK',
     long_description=long_description,
