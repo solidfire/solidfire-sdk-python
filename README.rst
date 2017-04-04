631006c43f0f804a9ee9f0abfb7dacd46cb8c3c7
Build Automation <dan.audette@solidfire.com>
2017-04-04 13:44:34 -0600

    {"pythonVersion":"1.2.2.156", "descriptorVersion":"1.0.0.40", "generatorVersion": "1.0.0.10"}

--- README.rst
+++ README.rst
@@ -8,7 +8,7 @@ Python SDK library for interacting with SolidFire Element API
 Current Release
 ---------------
 
-Version 1.2.2.155
+Version 1.2.2.156
 
 Description
 -----------
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.2.2.155',
+    version='1.2.2.156',
 
     description='SolidFire Python SDK',
     long_description=long_description,
