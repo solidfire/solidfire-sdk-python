820b2d91a18b5cd435d3be4954fb0d85ee63bdee
Adam Haid <adam.haid@solidfire.com>
2017-06-13 10:23:24 -0600

    bump version to 1.5.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2016, NetApp, Inc.  All Rights Reserved.'
 author = 'Adam Haid'
-version = '1.4.0.274'
-release = '1.4.0.274'
+version = '1.5.0.1'
+release = '1.5.0.1'
 language = 'en'
 
 exclude_patterns = ['_build']
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.4.0.274',
+    version='1.5.0.1',
 
     description='SolidFire Python SDK',
     long_description=long_description,
