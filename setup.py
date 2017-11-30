af59ed81b424e8209f978ec1d4a4fd775e143444
Build Automation <nicole.cotton@netapp.com>
2017-11-29 17:11:44 -0700

    {"pythonVersion":"1.5.0.91", "descriptorVersion":"1.5.0.37", "generatorVersion": "1.5.0.74"}

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class Clean(_clean):
 setup(
     name='solidfire-sdk-python',
 
-    version='1.5.0.90',
+    version='1.5.0.91',
 
     description='SolidFire Python SDK',
     long_description=long_description,
