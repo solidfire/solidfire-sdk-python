686097128aa434f479067fce8e121bbd83510fbf
Build Automation <nicole.cotton@netapp.com>
2018-02-20 10:33:45 -0700

    {"pythonVersion":"1.6.0.16", "descriptorVersion":"1.6.0.7", "generatorVersion": "1.6.0.8"}

--- conf.py
+++ conf.py
@@ -18,8 +18,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2017, NetApp, Inc.  All Rights Reserved.'
 author = 'Adam Haid'
-version = '1.6.0.15'
-release = '1.6.0.15'
+version = '1.6.0.16'
+release = '1.6.0.16'
 language = 'en'
 
 exclude_patterns = ['_build']
