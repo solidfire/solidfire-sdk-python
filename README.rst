a88365d83e326f1cf81fff0c053c4f7663995a66
Build Automation <nicole.cotton@netapp.com>
2018-04-05 12:08:53 -0600

    {"pythonVersion":"1.6.0.36", "descriptorVersion":"1.6.0.15", "generatorVersion": "1.6.0.18"}

--- README.rst
+++ README.rst
@@ -8,7 +8,7 @@ Python SDK library for interacting with SolidFire Element API
 Current Release
 ---------------
 
-Version 1.6.0.35
+Version 1.6.0.36
 
 Description
 -----------
--- conf.py
+++ conf.py
@@ -18,8 +18,8 @@ master_doc = 'index'
 project = 'solidfire-sdk-python'
 copyright = '2014-2017, NetApp, Inc.  All Rights Reserved.'
 author = 'Adam Haid'
-version = '1.6.0.35'
-release = '1.6.0.35'
+version = '1.6.0.36'
+release = '1.6.0.36'
 language = 'en'
 
 exclude_patterns = ['_build']
